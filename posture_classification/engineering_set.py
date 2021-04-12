4f22921081ed8e6b733f96dfa08534ba99be2a28
Benjamin Griffiths <drjamin1990@gmail.com>
2021-04-12 13:29:20 +0100

    completed basic processing of engineering set

--- posture_classification/engineering_set.py
+++ posture_classification/engineering_set.py
@@ -1,4 +1,4 @@
-from algorithm_dataset_abc import ABCDataset
+from processing_dataset_abc import ABCDataset
 
 import pandas as pd
 import numpy as np
@@ -12,8 +12,15 @@ class EngineeringSet(ABCDataset):
         self.raw_acceleration_data = None
         self.posture_stack = None
 
-    def show_dataset(self):
+    def show_set(self):
         print('Engineering Set')
+        print('----------')
+        print('Engineering set data')
+        print(self.dataset[0])
+        print('----------')
+        print('Posture Classes')
+        print(self.dataset[1])
+        print('----------')
 
     def get_data(self, activity_monitor):
         self.raw_acceleration_data = activity_monitor.raw_data
@@ -43,10 +50,22 @@ class EngineeringSet(ABCDataset):
             # Print New Line on Complete
             if iteration == total: 
                 print()
-    
+        
+        # Creat empty engineering set
+        engineering_set = np.empty((0,295,3), int)
+        posture_class = []
         # Load in the accelerometer data (to chunk or not to chunk?)
         #raw_acceleration_data =  pd.read_csv(self.raw_acceleration_data, skiprows=[1],  sep=';')
+        counter = 0
+        max_counter = 99
         for chunk in pd.read_csv(self.raw_acceleration_data, chunksize=100000):
+            # Contitions for ending things early
+            if counter <= max_counter:
+                printProgressBar (counter, max_counter, 'Engineering set progress:')
+                counter += 1
+            else:
+                break
+
             chunk = chunk['sep=;'].apply(lambda x: pd.Series(x.split(';')))
             chunk.columns = ["Time", "Index", "X", "Y", "Z"]
             try:
@@ -62,25 +81,43 @@ class EngineeringSet(ABCDataset):
             for index, row in self.posture_stack.iterrows():
                 # If the epoch start time is less than the first value in data then look at next epoch
                 if row.Start_Time < chunk.Time.iloc[0]:
-                    print('epoch start time is less than the first value in data')
-                    breakpoint()
+                    #print('epoch start time is less than the first value in data')
+                    continue
                 # If the epoch start time is in the dataset and the end time is in the dataset then extract the data
-                elif row.Start_Time >= chunk.Time.iloc[0] & row.Finish_Time <= chunk.Time.iloc[-1]:
+                elif row.Start_Time >= chunk.Time.iloc[0] and row.Finish_Time <= chunk.Time.iloc[-1]:
                     current_epoch = chunk[(chunk.Time >= row.Start_Time) & (chunk.Time <= row.Finish_Time)].copy()
-                    print('data found')
                 # If the start time is in the dataset but the end time is not in the dataset then load in the next dataset
-                elif row.Start_Time >= chunk.Time.iloc[0] & row.Finish_Time > chunk.Time.iloc[-1]:
-                    print('epoch end time is not in the dataset')
-                    breakpoint()
+                elif row.Start_Time >= chunk.Time.iloc[0] and row.Finish_Time > chunk.Time.iloc[-1]:
+                    print('found epoch start time but epoch end time is outside of the dataset')
+                    #breakpoint()
+                    break
                 # If the start time is greater than the last value in the dataset then load in the next dataset 
                 elif row.Start_Time > chunk.Time.iloc[-1]:
                     print('epoch start time is greater than the last value in data')
-                    breakpoint()
-                # Assign the accelerometer data to a tensor index
-                current_epoch_data = current_epoch[['X','Y','Z']].to_numpy()
-                arr = np.array([[[], []], [[], []]])
+                    #breakpoint()
+                    break
 
-        # Create a var containing the posture classes
+                # Assign the accelerometer data to a tensor index (in numpy form, convert to tf later)
+                current_epoch_accel_data = current_epoch[['X','Y','Z']].to_numpy()
+                engineering_set = np.append(engineering_set, [current_epoch_accel_data[:295,:]], axis=0)
+                # Assign the corresponding event code to a posture class list
+                posture_class.append(row.Event_Code)
+
+        posture_class = np.array(posture_class)
+        self.dataset = [engineering_set, posture_class]
+        self.remove_classes()
+
+    def remove_classes(self, classes_to_remove = []):
+        # remove the default non pure classes
+        classes_to_keep = self.dataset[1] != 99
+        for classes in classes_to_remove:
+            classes_to_keep = classes_to_keep + (self.dataset[1] != classes)
+        self.dataset[1] = self.dataset[1][classes_to_keep]
+        self.dataset[0] = self.dataset[0][classes_to_keep]
 
     def save_set(self):
-        print("Make me work too!")
+        print('...saving engineering set')
+        np.save('engineering_set.npy', self.dataset[0])
+        np.save('engineering_set_classes.npy', self.dataset[1])
+        # example for loading data
+        #np.load('engineering_set.npy')
