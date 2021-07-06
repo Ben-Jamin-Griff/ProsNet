376c0125d9dba15c1fff6cc2544d8e511f58fe22
Benjamin Griffiths <drjamin1990@gmail.com>
2021-07-06 18:07:57 +0100

    getting acc counts fixed for non wear algorithm

---
+++ classify/non_wear_processing.py
@@ -0,0 +1,23 @@
+from activity_monitor.activpal import Activpal
+from stack.epoch_stack import EpochStack
+from stack.non_wear_stack import NonWearStack
+
+"""
+Analyse Non-wear Data
+
+This script provides..
+"""
+
+activPal = Activpal()
+activPal.load_raw_data()
+#activPal.load_event_data()
+
+non_wear_stack = NonWearStack()
+non_wear_stack.get_data(activPal)
+non_wear_stack.create_stack()
+non_wear_stack.show_stack()
+
+#posture_stack = EpochStack()
+#posture_stack.get_data(activPal)
+#posture_stack.create_stack(stack_type = 'pure')
+#posture_stack.show_stack()
--- classify/stack/non_wear_stack.py
+++ classify/stack/non_wear_stack.py
@@ -5,8 +5,12 @@ import pandas as pd
 import numpy as np
 import math
 import datetime
+from scipy import signal
+import resampy
 
 from uos_activpal.io.raw import load_activpal_data
+import warnings
+warnings.filterwarnings("ignore")
 
 class NonWearStack(ABCPostureStack, Helper):
     def __init__(self, processing_type='epoch'):
@@ -29,7 +33,7 @@ class NonWearStack(ABCPostureStack, Helper):
         print(f"The posture stacks contains {self.posture_stack_duration} seconds of data.")
         print('----------')
 
-    def create_non_wear_stack(self, epochSize = 1):
+    def create_stack(self, epochSize = 1):
         """
         epochSize = 1 for 1 second epochs
         """
@@ -40,10 +44,126 @@ class NonWearStack(ABCPostureStack, Helper):
         x = signals[:total_samples,0]
         y = signals[:total_samples,1]
         z = signals[:total_samples,2]
-        chunk = pd.DataFrame({'Time':arr, 'X':x, 'Y':y, 'Z':z})
+        x_g = ((x/253)-0.5)*4
+        y_g = ((y/253)-0.5)*4
+        z_g = ((z/253)-0.5)*4
 
+        ##predefined filter coefficients, as found by Jan Brond
+        filesf = 20
 
-        if self.processing_type == 'epoch':
+        A_coeff = np.array(
+            [1, -4.1637, 7.5712,-7.9805, 5.385, -2.4636, 0.89238, 0.06361, -1.3481, 2.4734, -2.9257, 2.9298, -2.7816, 2.4777,
+            -1.6847, 0.46483, 0.46565, -0.67312, 0.4162, -0.13832, 0.019852])
+        B_coeff = np.array(
+            [0.049109, -0.12284, 0.14356, -0.11269, 0.053804, -0.02023, 0.0063778, 0.018513, -0.038154, 0.048727, -0.052577,
+            0.047847, -0.046015, 0.036283, -0.012977, -0.0046262, 0.012835, -0.0093762, 0.0034485, -0.00080972, -0.00019623])
+
+        def pptrunc(data, max_value):
+            '''
+            Saturate a vector such that no element's absolute value exceeds max_abs_value.
+            Current name: absolute_saturate().
+            :param data: a vector of any dimension containing numerical data
+            :param max_value: a float value of the absolute value to not exceed
+            :return: the saturated vector
+            '''
+            outd = np.where(data > max_value, max_value, data)
+            return np.where(outd < -max_value, -max_value, outd)
+
+        def trunc(data, min_value):
+        
+            '''
+            Truncate a vector such that any value lower than min_value is set to 0.
+            Current name zero_truncate().
+            :param data: a vector of any dimension containing numerical data
+            :param min_value: a float value the elements of data should not fall below
+            :return: the truncated vector
+            '''
+
+            return np.where(data < min_value, 0, data)
+
+        def runsum(data, length, threshold):
+            '''
+            Compute the running sum of values in a vector exceeding some threshold within a range of indices.
+            Divides the data into len(data)/length chunks and sums the values in excess of the threshold for each chunk.
+            Current name run_sum().
+            :param data: a 1D numerical vector to calculate the sum of
+            :param len: the length of each chunk to compute a sum along, as a positive integer
+            :param threshold: a numerical value used to find values exceeding some threshold
+            :return: a vector of length len(data)/length containing the excess value sum for each chunk of data
+            '''
+            
+            N = len(data)
+            cnt = int(math.ceil(N/length))
+
+            rs = np.zeros(cnt)
+
+            for n in range(cnt):
+                for p in range(length*n, length*(n+1)):
+                    if p<N and data[p]>=threshold:
+                        rs[n] = rs[n] + data[p] - threshold
+
+            return rs
+
+        def counts(data, filesf, B=B_coeff, A=A_coeff):
+            '''
+            Get activity counts for a set of accelerometer observations.
+            First resamples the data frequency to 30Hz, then applies a Butterworth filter to the signal, then filters by the
+            coefficient matrices, saturates and truncates the result, and applies a running sum to get the final counts.
+            Current name get_actigraph_counts()
+            :param data: the vertical axis of accelerometer readings, as a vector
+            :param filesf: the number of observations per second in the file
+            :param a: coefficient matrix for filtering the signal, as found by Jan Brond
+            :param b: coefficient matrix for filtering the signal, as found by Jan Brond
+            :return: a vector containing the final counts
+            '''
+            
+            deadband = 0.068
+            sf = 30
+            peakThreshold = 2.13
+            adcResolution = 0.0164
+            integN = 10
+            gain = 0.965
+
+            #if filesf>sf:
+            data = resampy.resample(np.asarray(data), filesf, sf)
+
+            B2, A2 = signal.butter(4, np.array([0.01, 7])/(sf/2), btype='bandpass')
+            dataf = signal.filtfilt(B2, A2, data)
+
+            B = B * gain
+
+            #NB: no need for a loop here as we only have one axis in array
+            fx8up = signal.lfilter(B, A, dataf)
+
+            fx8 = pptrunc(fx8up[::3], peakThreshold) #downsampling is replaced by slicing with step parameter
+
+            return runsum(np.floor(trunc(np.abs(fx8), deadband)/adcResolution), integN, 0)
+
+        # calculate counts per axis
+        c_x = counts(x_g, filesf)
+        c_y = counts(y_g, filesf)
+        c_z = counts(z_g, filesf)
+        c_vm = np.sqrt(c_x**2 + c_y**2 + c_z**2)
+
+        minutes = total_time.total_seconds()/60
+        #timestep = minutes/len(c_vm)
+        time_arr = np.linspace(0, minutes, num=len(c_vm))
+
+        chunk = pd.DataFrame({'Time':time_arr, 'X':c_x, 'Y':c_y, 'Z':c_z, 'VM':c_vm})
+        chunk = chunk.astype(int)
+
+        breakpoint()
+
+        NonWear = 0 # identifier for logical sorting
+        Wear = 1 # identifier for logical sorting
+
+        previousEpochClassification = 0 # The algorithm assumes non-wear prior to the start of recording
+
+        amplitudeSensitivity = 15 # original value = 15 % Relates to STEP 3: threshold under which the activity counts will be automatically assumed to be non-wear if detected within a period classified as non-wear
+        timeSensitivity = 60 # original value = 5 % Relates to STEP 3: if 2 spikes of activity greater than 'amplitudeSensitivity' occur within 'timeSensitivity' minutes of each other this will be classified as wear 
+        extraCheckTime = 60
+
+        if chunk:
             event_data = pd.read_csv(self.events_to_process)
 
             event_data.Time = pd.to_datetime(event_data.Time, unit='d', origin='1899-12-30')
