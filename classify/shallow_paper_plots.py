14df7f3db0b5d8b00d446c6d2420037495b37232
Benjamin Griffiths <drjamin1990@gmail.com>
2021-08-15 09:33:35 +0100

    created acceleration plots

--- classify/shallow_paper_plots.py
+++ classify/shallow_paper_plots.py
@@ -1,3 +1,7 @@
+from activity_monitor.activpal import Activpal
+from stack.epoch_stack import EpochStack
+from dataset.engineering_set import EngineeringSet
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,57 +13,83 @@ Running shallow paper plots
 This script creates the plots for the shallow paper...
 """
 
-# Collecting the datasets
+epoch_sizes = [15]
 
-file_dir = './apc-data/processed-5000-events/'
+raw_data_paths = [
+    "./apc-data/bg2-data/shank-AP472387 202a 19Sep20 1-00pm for 2d 15m-CREA-PA08110254-AccelDataUncompressed.csv",
+]
 
-feature_set = []
-posture_set = []
-participant_ids = []
+event_data_paths = [
+    "./apc-data/bg2-data/thigh-AP870085 202a 19Sep20 1-00pm for 2d 17m-CREA-PA08110254-Events.csv",
+    ]
 
-EPOCH_SIZES = [5, 15, 30, 60, 120, 180]
+# Load in each participant's data
+activPal = Activpal()
+activPal.load_raw_data(raw_data_paths[0])
+activPal.load_event_data(event_data_paths[0])
 
-for epoch_size in EPOCH_SIZES:
-  feature_set.append(np.load(file_dir + 'set_size_' + str(epoch_size) + '_feature_set.npy'))
-  posture_set.append(np.load(file_dir + 'set_size_' + str(epoch_size) + '_feature_set_classes.npy'))
-  participant_ids.append(np.load(file_dir + 'set_size_' + str(epoch_size) + '_feature_set_ids.npy'))
+posture_stack = EpochStack()
+posture_stack.get_data(activPal)
+posture_stack.create_stack(stack_type = 'pure', subset_of_data = 100)
 
-# Combining similar activity codes
+engineering_set = EngineeringSet()
+engineering_set.get_data(activPal)
+engineering_set.get_posture_stack(posture_stack)
+engineering_set.create_set()
 
-for epoch, dataset in enumerate(posture_set):
-  for count, value in enumerate(dataset):
-    if dataset[count] == 2.1:
-      dataset[count] = 2
-    elif dataset[count] == 3.1:
-      dataset[count] = 3
-    elif dataset[count] == 3.2:
-      dataset[count] = 3
-    else:
-      continue
-  posture_set[epoch] = dataset
+engineering_set.dataset[0][0]
+engineering_set.dataset[1][0]
 
-# Removing specific activity codes from dataset
+counter = 0
+for data, posture in zip(engineering_set.dataset[0], engineering_set.dataset[1]):
+    if posture == 0:
+        counter =+ 1
+        plt.plot(data)
+        if counter == 10:
+            plt.title('Sitting')
+            plt.ion()
+            plt.show()
+            plt.savefig('Sitting_example.png')
+            plt.close()
+            break
 
-for count, epoch in enumerate(EPOCH_SIZES):
-    for class_to_remove in [4,5]:
-        keep_idx = posture_set[count] != class_to_remove
-        posture_set[count] = posture_set[count][keep_idx]
-        feature_set[count] = feature_set[count][keep_idx]
-        participant_ids[count] = participant_ids[count][keep_idx]
+counter = 0
+for data, posture in zip(engineering_set.dataset[0], engineering_set.dataset[1]):
+    if posture == 1:
+        counter =+ 1
+        plt.plot(data)
+        if counter == 10:
+            plt.title('Standing')
+            plt.ion()
+            plt.show()
+            plt.savefig('Standing_example.png')
+            plt.close()
+            break
 
-# How many participants are in the datasets
+counter = 0
+for data, posture in zip(engineering_set.dataset[0], engineering_set.dataset[1]):
+    if posture == 2:
+        counter =+ 1
+        plt.plot(data)
+        if counter == 10:
+            plt.title('Stepping')
+            plt.ion()
+            plt.show()
+            plt.savefig('Stepping_example.png')
+            plt.close()
+            break
 
-print('Number of participants')
-print(len(np.unique(participant_ids[0])))
+counter = 0
+for data, posture in zip(engineering_set.dataset[0], engineering_set.dataset[1]):
+    if posture == 3:
+        counter =+ 1
+        plt.plot(data)
+        if counter == 10:
+            plt.title('Lying')
+            plt.ion()
+            plt.show()
+            plt.savefig('Lying_example.png')
+            plt.close()
+            break
 
-for count, epoch in enumerate(EPOCH_SIZES):
-
-    print('Creating plots with epoch size ' + str(epoch))
-
-    analysis_feature_set = feature_set[count].copy()
-    analysis_posture_set = posture_set[count].copy()
-    analysis_participant_ids = participant_ids[count].copy()
-
-    breakpoint()
-
-    print('El Finisimo...')
+print('El Finisimo...')
