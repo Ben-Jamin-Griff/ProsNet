41a8013827f48ff6c0b5045ce9c0c6467267dd95
Benjamin Griffiths <drjamin1990@gmail.com>
2021-08-17 11:44:45 +0100

    updated plots

--- classify/shallow_paper_plots.py
+++ classify/shallow_paper_plots.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statistics
 
 """
 Running shallow paper plots
@@ -16,12 +17,12 @@ This script creates the plots for the shallow paper...
 epoch_sizes = [15]
 
 raw_data_paths = [
-    "./apc-data/bg2-data/shank-AP472387 202a 19Sep20 1-00pm for 2d 15m-CREA-PA08110254-AccelDataUncompressed.csv",
+    "./apc-data/af-data/AF_Shin-AP971770 202a 28May21 3-28pm for 16d 20h 54m-CREA-PA08110254-AccelDataUncompressed.csv",
 ]
 
 event_data_paths = [
-    "./apc-data/bg2-data/thigh-AP870085 202a 19Sep20 1-00pm for 2d 17m-CREA-PA08110254-Events.csv",
-    ]
+    "./apc-data/af-data/AF_Thigh-AP971728 202a 28May21 3-24pm for 16d 21h-CREA-PA08110254-Events.csv",
+]
 
 # Load in each participant's data
 activPal = Activpal()
@@ -30,62 +31,83 @@ activPal.load_event_data(event_data_paths[0])
 
 posture_stack = EpochStack()
 posture_stack.get_data(activPal)
-posture_stack.create_stack(stack_type = 'pure', subset_of_data = 100)
+posture_stack.create_stack(stack_type = 'pure', subset_of_data = 1000)
 
 engineering_set = EngineeringSet()
 engineering_set.get_data(activPal)
 engineering_set.get_posture_stack(posture_stack)
 engineering_set.create_set()
 
-engineering_set.dataset[0][0]
-engineering_set.dataset[1][0]
+final_count = 6 # 3 works well for this value but test out some valuse
+
+float_range_array = np.arange(0, 15, 15/295)
+#float_range_list = list(float_range_array)
 
 counter = 0
+data_ensemble = np.empty([295, 3])
 for data, posture in zip(engineering_set.dataset[0], engineering_set.dataset[1]):
     if posture == 0:
-        counter =+ 1
-        plt.plot(data)
-        if counter == 10:
-            plt.title('Sitting')
-            plt.show()
+        counter += 1
+        data_ensemble = data_ensemble + data
+        if counter == final_count:
+            data_ensemble = data_ensemble / final_count
+            data_ensemble = ((data_ensemble/253)-0.5)*4
+            plt.plot(np.arange(0, 15, 15/295), data_ensemble)
+            plt.ylabel('Acceleration gs')
+            plt.xlabel('Seconds')
+            #plt.title('Sitting')
             plt.savefig('Sitting_example.png')
-            plt.close()
             break
+plt.close()
 
 counter = 0
+data_ensemble = np.empty([295, 3])
 for data, posture in zip(engineering_set.dataset[0], engineering_set.dataset[1]):
     if posture == 1:
-        counter =+ 1
-        plt.plot(data)
-        if counter == 10:
-            plt.title('Standing')
-            plt.show()
+        counter += 1
+        data_ensemble = data_ensemble + data
+        if counter == final_count:
+            data_ensemble = data_ensemble / final_count
+            data_ensemble = ((data_ensemble/253)-0.5)*4
+            plt.plot(np.arange(0, 15, 15/295), data_ensemble)
+            plt.ylabel('Acceleration gs')
+            plt.xlabel('Seconds')
+            #plt.title('Standing')
             plt.savefig('Standing_example.png')
-            plt.close()
             break
+plt.close()
 
 counter = 0
+data_ensemble = np.empty([295, 3])
 for data, posture in zip(engineering_set.dataset[0], engineering_set.dataset[1]):
     if posture == 2:
-        counter =+ 1
-        plt.plot(data)
-        if counter == 10:
-            plt.title('Stepping')
-            plt.show()
+        counter += 1
+        data_ensemble = data_ensemble + data
+        if counter == final_count:
+            data_ensemble = data_ensemble / final_count
+            data_ensemble = ((data_ensemble/253)-0.5)*4
+            plt.plot(np.arange(0, 15, 15/295), data_ensemble)
+            plt.ylabel('Acceleration gs')
+            plt.xlabel('Seconds')
+            #plt.title('Stepping')
             plt.savefig('Stepping_example.png')
-            plt.close()
             break
+plt.close()
 
 counter = 0
 for data, posture in zip(engineering_set.dataset[0], engineering_set.dataset[1]):
     if posture == 3:
-        counter =+ 1
-        plt.plot(data)
-        if counter == 10:
-            plt.title('Lying')
-            plt.show()
+        counter += 1
+        data_ensemble = data_ensemble + data
+        if counter == final_count:
+            data_ensemble = data_ensemble / final_count
+            data_ensemble = ((data_ensemble/253)-0.5)*4
+            plt.plot(np.arange(0, 15, 15/295), data_ensemble)
+            plt.ylabel('Acceleration (g)')
+            plt.xlabel('Seconds')
+            #plt.title('Lying')
             plt.savefig('Lying_example.png')
-            plt.close()
             break
+plt.close()
 
 print('El Finisimo...')
