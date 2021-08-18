0c4443b74e25e591b526172f87127c417e6c260d
Benjamin Griffiths <drjamin1990@gmail.com>
2021-08-18 11:04:09 +0100

    updating plots

--- classify/shallow_paper_plots.py
+++ classify/shallow_paper_plots.py
@@ -6,7 +6,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import statistics
+import plotly.express as px
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 
 """
 Running shallow paper plots
@@ -14,100 +19,101 @@ Running shallow paper plots
 This script creates the plots for the shallow paper...
 """
 
+def plot_ensemble(posture_code, posture_label, final_count = 3):
+    counter = 0
+    data_ensemble = np.empty([295, 3])
+    for data, posture in zip(engineering_set.dataset[0], engineering_set.dataset[1]):
+        if posture == posture_code:
+            counter += 1
+            data_ensemble = data_ensemble + data
+            if counter == final_count:
+                data_ensemble = data_ensemble / final_count
+                data_ensemble = ((data_ensemble/253)-0.5)*4
+                plt.plot(np.arange(0, 15, 15/295), data_ensemble)
+                plt.ylabel('Acceleration gs')
+                plt.xlabel('Seconds')
+                plt.savefig(posture_label + '_ensemble.png')
+                break
+    plt.close()
+
+def plot_postures(posture_code, posture_label, final_count = 3):
+    counter = 0
+    for data, posture in zip(engineering_set.dataset[0], engineering_set.dataset[1]):
+        if posture == posture_code:
+            counter += 1
+            plt.plot(np.arange(0, 15, 15/295), data)
+            if counter == final_count:
+                plt.ylabel('Acceleration gs')
+                plt.xlabel('Seconds')
+                plt.savefig(posture_label + '_postures.png')
+                break
+    plt.close()
+
+'''
 epoch_sizes = [15]
-
-raw_data_paths = [
-    "./apc-data/af-data/AF_Shin-AP971770 202a 28May21 3-28pm for 16d 20h 54m-CREA-PA08110254-AccelDataUncompressed.csv",
-]
-
-event_data_paths = [
-    "./apc-data/af-data/AF_Thigh-AP971728 202a 28May21 3-24pm for 16d 21h-CREA-PA08110254-Events.csv",
-]
-
+raw_data_paths = ["./apc-data/af-data/AF_Shin-AP971770 202a 28May21 3-28pm for 16d 20h 54m-CREA-PA08110254-AccelDataUncompressed.csv"]
+event_data_paths = ["./apc-data/af-data/AF_Thigh-AP971728 202a 28May21 3-24pm for 16d 21h-CREA-PA08110254-Events.csv"]
 # Load in each participant's data
 activPal = Activpal()
 activPal.load_raw_data(raw_data_paths[0])
 activPal.load_event_data(event_data_paths[0])
-
 posture_stack = EpochStack()
 posture_stack.get_data(activPal)
-posture_stack.create_stack(stack_type = 'pure', subset_of_data = 1000)
-
+posture_stack.create_stack(stack_type = 'pure', subset_of_data = 100)
 engineering_set = EngineeringSet()
 engineering_set.get_data(activPal)
 engineering_set.get_posture_stack(posture_stack)
 engineering_set.create_set()
 
-final_count = 6 # 3 works well for this value but test out some valuse
+plot_ensemble(0, 'Sitting')
+plot_ensemble(1, 'Standing')
+plot_ensemble(2, 'Stepping')
+plot_ensemble(3, 'Lying')
 
-float_range_array = np.arange(0, 15, 15/295)
-#float_range_list = list(float_range_array)
+plot_postures(0, 'Sitting')
+plot_postures(1, 'Standing')
+plot_postures(2, 'Stepping')
+plot_postures(3, 'Lying')
+'''
 
-counter = 0
-data_ensemble = np.empty([295, 3])
-for data, posture in zip(engineering_set.dataset[0], engineering_set.dataset[1]):
-    if posture == 0:
-        counter += 1
-        data_ensemble = data_ensemble + data
-        if counter == final_count:
-            data_ensemble = data_ensemble / final_count
-            data_ensemble = ((data_ensemble/253)-0.5)*4
-            plt.plot(np.arange(0, 15, 15/295), data_ensemble)
-            plt.ylabel('Acceleration gs')
-            plt.xlabel('Seconds')
-            #plt.title('Sitting')
-            plt.savefig('Sitting_example.png')
-            break
-plt.close()
+# Orginal (nice looking plots)
+epoch_sizes = [15]
+raw_data_paths = ["./apc-data/bg2-data/shank-AP472387 202a 19Sep20 1-00pm for 2d 15m-CREA-PA08110254-AccelDataUncompressed.csv"]
+event_data_paths = ["./apc-data/bg2-data/thigh-AP870085 202a 19Sep20 1-00pm for 2d 17m-CREA-PA08110254-Events.csv"]
+# Load in each participant's data
+activPal = Activpal()
+activPal.load_raw_data(raw_data_paths[0])
+activPal.load_event_data(event_data_paths[0])
+posture_stack = EpochStack()
+posture_stack.get_data(activPal)
+posture_stack.create_stack(stack_type = 'pure', subset_of_data = 500)
+engineering_set = EngineeringSet()
+engineering_set.get_data(activPal)
+engineering_set.get_posture_stack(posture_stack)
+engineering_set.create_set()
 
-counter = 0
-data_ensemble = np.empty([295, 3])
-for data, posture in zip(engineering_set.dataset[0], engineering_set.dataset[1]):
-    if posture == 1:
-        counter += 1
-        data_ensemble = data_ensemble + data
-        if counter == final_count:
-            data_ensemble = data_ensemble / final_count
-            data_ensemble = ((data_ensemble/253)-0.5)*4
-            plt.plot(np.arange(0, 15, 15/295), data_ensemble)
-            plt.ylabel('Acceleration gs')
-            plt.xlabel('Seconds')
-            #plt.title('Standing')
-            plt.savefig('Standing_example.png')
-            break
-plt.close()
+plot_ensemble(0, 'Nice_Sitting')
+plot_ensemble(1, 'Nice_Standing')
+plot_ensemble(2, 'Nice_Stepping')
+plot_ensemble(3, 'Nice_Lying')
 
-counter = 0
-data_ensemble = np.empty([295, 3])
-for data, posture in zip(engineering_set.dataset[0], engineering_set.dataset[1]):
-    if posture == 2:
-        counter += 1
-        data_ensemble = data_ensemble + data
-        if counter == final_count:
-            data_ensemble = data_ensemble / final_count
-            data_ensemble = ((data_ensemble/253)-0.5)*4
-            plt.plot(np.arange(0, 15, 15/295), data_ensemble)
-            plt.ylabel('Acceleration gs')
-            plt.xlabel('Seconds')
-            #plt.title('Stepping')
-            plt.savefig('Stepping_example.png')
-            break
-plt.close()
+plot_postures(0, 'Nice_Sitting')
+plot_postures(1, 'Nice_Standing')
+plot_postures(2, 'Nice_Stepping')
+plot_postures(3, 'Nice_Lying')
 
-counter = 0
-for data, posture in zip(engineering_set.dataset[0], engineering_set.dataset[1]):
-    if posture == 3:
-        counter += 1
-        data_ensemble = data_ensemble + data
-        if counter == final_count:
-            data_ensemble = data_ensemble / final_count
-            data_ensemble = ((data_ensemble/253)-0.5)*4
-            plt.plot(np.arange(0, 15, 15/295), data_ensemble)
-            plt.ylabel('Acceleration (g)')
-            plt.xlabel('Seconds')
-            #plt.title('Lying')
-            plt.savefig('Lying_example.png')
-            break
-plt.close()
+'''
+app = dash.Dash(__name__)
 
-print('El Finisimo...')
+app.layout = html.Div([
+    dcc.Graph(id="line-chart"),
+])
+
+@app.callback(
+    Output("line-chart", "figure"))
+def update_line_chart():
+    fig = px.line(engineering_set.dataset[0], color=engineering_set.dataset[1])
+    return fig
+
+app.run_server(debug=True)
+'''
