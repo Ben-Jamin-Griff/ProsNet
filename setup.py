f1f7642550254a2a4070f5630625fa50e59b3aac
Benjamin Griffiths <drjamin1990@gmail.com>
2021-10-20 21:54:48 +0100

    fixing dependancy issue for package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = 'ProsNet',
+    version = '0.0.2',
+    author = 'Benjamin Griffiths',
+    description = 'A package for processing activPAL activity monitor data.',
+    py_modules=["ProsNet"],
+    package_dir={'': 'src'},
+    install_requires = [
+        "requests",
+        "pandas",
+        "matplotlib",
+        "scipy",
+        "uos-activpal",
+        "numba",
+        "seaborn",
+        "numpy == 1.20"
+    ],
+)
