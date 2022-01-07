5836bc995aaf66cf4f691547015886607328ce21
Benjamin Griffiths <drjamin1990@gmail.com>
2022-01-07 14:03:00 +0000

    update package config files

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name = 'ProsNet',
-    version = '0.0.3',
+    version = '0.0.4',
     author = 'Benjamin Griffiths',
     description = 'A package for processing activPAL activity monitor data.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ben-Jamin-Griff/ProsNet",
+    project_urls={
+        "Bug Tracker": "https://github.com/Ben-Jamin-Griff/ProsNet/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     py_modules=["ProsNet"],
     package_dir={'': 'src'},
+    packages=setuptools.find_packages(where="src"),
+    python_requires = ">=3.6",
     install_requires = [
         "requests",
         "pandas",
